dbedb03116def521fe3b8d9b8dddd7b8470c6c63
debarkamondal <debarkamondal@gmail.com>
2023-01-23 01:51:05 +0530

    re: newsItem comp turned to func based comp

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -1,25 +1,22 @@
-import React, { Component } from 'react'
+import React from 'react'
 
-export class NewsItem extends Component {
+const NewsItem = (props) => {
 
-    render() {
-
-        let { title, description, imgUrl, newsUrl, source } = this.props;
-        return (
-            <div className="card my-2">
-                <span className="position-absolute top-0 translate-middle badge rounded-pill bg-danger" style={{left: '90%', zIndex: 1}}>
-                    {source}
-                    <span className="visually-hidden">unread messages</span>
-                </span>
-                <img src={imgUrl} className="card-img-top" alt="..." />
-                <div className="card-body">
-                    <h5 className="card-title">{title}</h5>
-                    <p className="card-text">{description}</p>
-                    <a href={newsUrl} target="_blank" rel="noreferrer" className="btn btn-primary">Read More</a>
-                </div>
-            </div >
-        )
-    }
+    let { title, description, imgUrl, newsUrl, source } = props;
+    return (
+        <div className="card my-2">
+            <span className="position-absolute top-0 translate-middle badge rounded-pill bg-danger" style={{ left: '90%', zIndex: 1 }}>
+                {source}
+                <span className="visually-hidden">unread messages</span>
+            </span>
+            <img src={imgUrl} className="card-img-top" alt="..." />
+            <div className="card-body">
+                <h5 className="card-title">{title}</h5>
+                <p className="card-text">{description}</p>
+                <a href={newsUrl} target="_blank" rel="noreferrer" className="btn btn-primary">Read More</a>
+            </div>
+        </div >
+    )
 }
 
 export default NewsItem
