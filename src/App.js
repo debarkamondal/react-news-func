722bf67740c9976249fcd4419827c724da7655e1
debarkamondal <debarkamondal@gmail.com>
2023-01-23 03:08:19 +0530

    re: app to function based component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 // import logo from './logo.svg';
 import './App.css';
-import React, { Component } from 'react'
+import React, {useState} from 'react'
 import NewsComponent from './components/NewsComponent';
 import LoadingBar from 'react-top-loading-bar'
 import RootLayout from './pages/RootLayout';
@@ -12,65 +12,61 @@ import {
 // import Navbar from './components/Navbar';
 
 
-export default class App extends Component {
-  apiKey= process.env.REACT_APP_NEWS_API
-  state = { progress: 0 }
-  setProgress = (newProgress) => {
-    this.setState({ progress: newProgress })
-    // console.log("This is from setProgress "+this.state.progress)
-  }
+const App = () => {
+  const apiKey= process.env.REACT_APP_NEWS_API
+  const [progress, setProgress] = useState(0)
 
-  router = createBrowserRouter([
+  const router = createBrowserRouter([
     {
       path: "/",
       element: <RootLayout />,
       children: [
         {
           index: true,
-          element: <NewsComponent apiKey={this.apiKey} setProgress={this.setProgress} key="general" pageSize={12} category="general" />
+          element: <NewsComponent apiKey={apiKey} setProgress={setProgress} key="general" pageSize={12} category="general" />
         },
         {
           path: "sports",
-          element: <NewsComponent apiKey={this.apiKey} setProgress={this.setProgress} key="sports" pageSize={12} category="sports" />,
+          element: <NewsComponent apiKey={apiKey} setProgress={setProgress} key="sports" pageSize={12} category="sports" />,
         },
         {
           path: "health",
-          element: <NewsComponent apiKey={this.apiKey} setProgress={this.setProgress} key="health" pageSize={12} category="health" />,
+          element: <NewsComponent apiKey={apiKey} setProgress={setProgress} key="health" pageSize={12} category="health" />,
         },
         {
           path: "general",
-          element: <NewsComponent apiKey={this.apiKey} setProgress={this.setProgress} key="general" pageSize={12} category="general" />
+          element: <NewsComponent apiKey={apiKey} setProgress={setProgress} key="general" pageSize={12} category="general" />
         },
         {
           path: "entertainment",
-          element: <NewsComponent apiKey={this.apiKey} setProgress={this.setProgress} key="entertainment" pageSize={12} category="entertainment" />,
+          element: <NewsComponent apiKey={apiKey} setProgress={setProgress} key="entertainment" pageSize={12} category="entertainment" />,
         },
         {
           path: "science",
-          element: <NewsComponent apiKey={this.apiKey} setProgress={this.setProgress} key="science" pageSize={12} category="science" />,
+          element: <NewsComponent apiKey={apiKey} setProgress={setProgress} key="science" pageSize={12} category="science" />,
         },
         {
           path: "business",
-          element: <NewsComponent apiKey={this.apiKey} setProgress={this.setProgress} key="business" pageSize={12} category="business" />,
+          element: <NewsComponent apiKey={apiKey} setProgress={setProgress} key="business" pageSize={12} category="business" />,
         },
         {
           path: "technology",
-          element: <NewsComponent apiKey={this.apiKey} setProgress={this.setProgress} key="technology" pageSize={12} category="technology" />,
+          element: <NewsComponent apiKey={apiKey} setProgress={setProgress} key="technology" pageSize={12} category="technology" />,
         }
       ]
     },
   ]);
-  render() {
+
     return (
       <>
-        <RouterProvider router={this.router} />
+        <RouterProvider router={router} />
         <LoadingBar
           color='#f11946'
-          progress={this.state.progress}
-          onLoaderFinished={() => this.setProgress(0)}
+          progress={progress}
+          onLoaderFinished={() => setProgress(0)}
           height = {3}
         />
       </>
     )
   }
-}
+  export default App
