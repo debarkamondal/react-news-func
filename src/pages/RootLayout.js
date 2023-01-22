96bb1ffeabcfda2e0d9abcc6016aae66096a6d5d
debarkamondal <debarkamondal@gmail.com>
2023-01-23 03:09:44 +0530

    re: refactor complete

--- src/pages/RootLayout.js
+++ src/pages/RootLayout.js
@@ -1,17 +1,14 @@
-import React, { Component } from 'react'
+import React from 'react'
 import Navbar from '../components/Navbar'
 import { Outlet } from 'react-router-dom'
 
-export class RootLayout extends Component {
-
-  render() {
-    return (
-      <>
-        <Navbar />        
-        <Outlet />
-      </>
-    )
-  }
+const RootLayout = () => {
+  return (
+    <>
+      <Navbar />
+      <Outlet />
+    </>
+  )
 }
 
 export default RootLayout
